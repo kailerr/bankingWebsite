efa83fd7d5f9ac3d17815ffd300e1a887dd2fd2c
Matt E <mathew.estrada@sjsu.edu>
2023-11-17 16:36:06 -0800

    Merge branch 'main' of https://github.com/kailerr/bankingWebsite into mattyBranch

--- js/home.js
+++ js/home.js
@@ -30,3 +30,40 @@ function openPopOutWindow() {
 
   window.open(url, 'PopOutWindow', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
 }
+
+function showGreetingMessage(userName) {
+  console.log("showGreetingMessage called with user:", userName);
+  const atmScreen = document.getElementById("atmScreen");
+  const buttonsContainer = document.getElementById("buttonsContainer");
+  const greetingMessage = document.getElementById("greetingMessage");
+
+  atmScreen.style.display = "none";
+  buttonsContainer.style.display = "block";
+
+  greetingMessage.textContent = `Hello, ${userName}! What would you like to do today?`;
+}
+
+function submit() {
+  const cardNumber = document.getElementById("cardNumber").value;
+  const pin = document.getElementById("pin").value;
+  showGreetingMessage("Frank Butt");
+
+  document.getElementById("cardBox").style.display = "block";
+  document.getElementById("buttonsContainer").style.display = "block";
+}
+
+document.getElementById("cancel").addEventListener("click", function () {
+  resetATM();
+});
+
+function resetATM() {
+  const atmScreen = document.getElementById("atmScreen");
+  const buttonsContainer = document.getElementById("buttonsContainer");
+
+  document.getElementById("cardNumber").value = "";
+  document.getElementById("pin").value = "";
+
+  atmScreen.style.display = "block";
+  cardBox.style.display = "none";
+  buttonsContainer.style.display = "none";
+}
